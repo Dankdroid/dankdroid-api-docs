10fcc9ff2abc1883e202dd567a49ddf396197cf8
Michael Tillmanns <=>
2021-05-11 19:34:20 +0200

    changed render path

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-raml2html --theme 'raml2html-modern-theme' -o '.\dankdroid-api-documentation.html' -i '.\dankdroid-api-documentation.raml'
+raml2html --theme 'raml2html-modern-theme' -o '.\index.html' -i '.\dankdroid-api-documentation.raml'
